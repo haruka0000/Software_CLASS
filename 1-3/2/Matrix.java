3a9e1e3dbd6db3bd538d439bbd519ae7b5704fb9
Haruka Takahashi <1124226@st.anan-nct.ac.jp>
2017-10-23 00:15:06 +0900

    2 finished

--- 1-3/2/Matrix.java
+++ 1-3/2/Matrix.java
@@ -1,11 +1,37 @@
 import java.io.*;
+import java.util.*;
+import java.text.Format;  //フォーマットのため
+import java.text.DateFormat;  // 日付のフォーマット用
 
 public class Matrix{
-  private int row;
-  private int col;
+  public String[] myName = {  // プリントヘッダー
+    "******************************",
+    "作成者：",
+    "日付：",
+    "入力ファイル名：",
+    "******************************"
+  };
+  
+  public void myPrint(String[] files){ // mainの最初に呼ばれる
+    Date now = new Date();  // Dateクラスのオブジェクト生成
+    Format fmt= DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG);  // 日付を日本語ロカールに従ってフォーマット
+    for (int i = 0 ; i < myName.length ; i++){
+      System.out.print(myName[i]);
+      switch (i) {
+        case 2: System.out.print(fmt.format(now)); break; // 日付
+        case 3: System.out.print(files[0] + ", " + files[1]); break;  // データファイル名
+      }
+        System.out.print('\n');
+    }
+  } 
 
-  private double[][] m;
- 
+
+  private int row;  // 行数
+  private int col;  // 列数
+
+  private double[][] m; // 行列
+
+  
   public int getRow(){
     return row;
   }
@@ -14,10 +40,11 @@ public class Matrix{
     return col;
   }
 
+  // ファイルを読み込み、行列に値を格納
   public static Matrix read(String filename){
-    Matrix mat = new Matrix();
+    Matrix mat = new Matrix();  // Matrix型オブジェクトの初期化
     try {
-      /* BufferedReaderクラスのオブジェクト生成 */
+      // BufferedReaderクラスのオブジェクト生成
       BufferedReader br = new BufferedReader(new FileReader(filename));
 
       String line;/* 行単位の読み込んだ文字列を保持 */
@@ -29,15 +56,20 @@ public class Matrix{
       mat.row = Integer.parseInt(row_col[0]);
       mat.col = Integer.parseInt(row_col[1]);
 
+      // 3 <= M,N,K <= 10を満たしていなければ終了 
+      if ((mat.row < 3) || (mat.row > 10)){
+        System.out.println("【ERROR】行列が 3 <= M,N,K <= 10 の条件を満たしていません");
+        System.exit(0);
+      }
+
       // 行列mを初期化
       mat.m = new double[mat.row][mat.col];
 
       int i = 0;
       int j = 0;
       while ((line = br.readLine()) != null){ // 行単位で処理
-        //System.out.println(line);
-        for(String str_d: line.split(" ", 0)){  // 分割して各行に追加
-          mat.m[i][j] = Double.parseDouble(str_d);
+        for(String str_d: line.split(" ", 0)){  // 1行をスペースで分割して各行（配列）に追加
+          mat.m[i][j] = Double.parseDouble(str_d);  // String型からDouble型に変換
           j++;
         }
         j = 0;
@@ -47,55 +79,63 @@ public class Matrix{
     catch(Exception e){
       e.printStackTrace();/* エラー処理 :トレース*/
     }
-    return mat;
+
+    return mat; // ファイルを読み込んだオブジェクトを返す
   } 
 
+  // 行列の中身を全て表示する
   public void print(){
-    for(int i = 0; i < row; i++){
+    for(int i = 0; i < getRow(); i++){
       System.out.print("| ");
-      for(int j = 0; j < col; j++){
-        System.out.print(m[i][j] + "\t");
+      for(int j = 0; j < getCol(); j++){
+        System.out.printf("%7f ", m[i][j]); // 小数点以下7桁で表示
       }
-      System.out.println("| ");
+      System.out.println("|");
     }
   }
   
+  // 行列の積を計算して返す
   public Matrix multiply(Matrix B){
-    if(col != B.row && row != B.col){
-      System.out.println("計算できません");
+    // Kが一致していない場合、計算できないと表示し終了する
+    if(getCol() != B.getRow() && getRow() != B.getCol()){
+      System.out.println("【ERROR】Kが一致しておらず計算できません");
+      System.exit(0);
     }
-    System.out.println(row);
-    System.out.println(B.col);
     
-    Matrix ANS = new Matrix();
-    ANS.row = row;
-    ANS.col = B.col;
-    ANS.m = new double[row][B.col];
     
-    for(int i = 0; i < row; i++){
-      for(int j = 0; j < B.col; j++){
-        for(int k = 0; k < col; k++){
+    Matrix ANS = new Matrix();  // 計算結果を格納するオブジェクトを初期化
+    ANS.row = getRow(); // Aの行を取得
+    ANS.col = B.getCol(); // Bの列数を取得
+    ANS.m = new double[getRow()][B.getCol()]; // 配列の初期化
+    
+    // 行列の積の計算
+    for(int i = 0; i < getRow(); i++){
+      for(int j = 0; j < B.getCol(); j++){
+        for(int k = 0; k < getCol(); k++){
           ANS.m[i][j] = ANS.m[i][j] + m[i][k] * B.m[k][j];
         }
       }
     }
-    System.out.println(ANS.m[0][0]);
 
-    return ANS;
+    return ANS; // 答えの行列を持つオブジェクトを返す
   }
+
+  // メイン
   public static void main(String[] args){
-    Matrix A = new Matrix().read(args[0]);
-    Matrix B = new Matrix().read(args[1]);
-    Matrix C = A.multiply(B);
-    System.out.println("入力行列, サイズ(" + A.getRow() + ", " + A.getCol() + ")");
+    // 作成者、日時、入力ファイルを表示するmyPrint()用のオブジェクト
+    Matrix mat = new Matrix();
+    mat.myPrint(args); // 作成者等を表示
+
+    Matrix A = new Matrix().read(args[0]);  // ファイルAを読み込み、オブジェクトの初期化
+    Matrix B = new Matrix().read(args[1]);  // ファイルBを読み込み、オブジェクトの初期化
+    Matrix C = A.multiply(B); // 行列の積を計算し、オブジェクトを格納
+
+    // 結果の表示
+    System.out.println("入力行列 A, サイズ：(" + A.getRow() + ", " + A.getCol() + ")");
     A.print();
-    System.out.println("入力行列, サイズ(" + B.getRow() + ", " + B.getCol() + ")");
+    System.out.println("入力行列 B, サイズ：(" + B.getRow() + ", " + B.getCol() + ")");
     B.print();
-    System.out.println("入力行列, サイズ(" + C.getRow() + ", " + C.getCol() + ")");
+    System.out.println("出力の積行列 C = A x B, サイズ：(" + C.getRow() + ", " + C.getCol() + ")");
     C.print();
   }
 }
-/* 
-    getRow(), getCol()に直す
-    名前の出力部分を作成する
-*/
