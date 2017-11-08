795b13a2c22469004b6531bf56a5ecc27a4cb390
Haruka Takahashi <1124226@st.anan-nct.ac.jp>
2017-11-08 22:51:41 +0900

    aaa

--- 1-1/test_sample/Q3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(void) {
-  char matrix[][6] = {"ABCDE", "abc"};
-  printf("%ld\n", sizeof(matrix[1]));
-  printf("%s\n", matrix[0]);
-  printf("%s\n", matrix[1]);
-}
